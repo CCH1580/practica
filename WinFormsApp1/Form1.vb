71136551ba0e97d421cd6d3af4e09c86259a2676
Hector Cruz <cch201300@gmail.com>
2023-09-07 12:00:47 -0600

    se agrega boton

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -8,4 +8,10 @@
         MsgBox("tu gfa en 4 :3")
 
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        MsgBox("tu gfa en 4 x2")
+
+    End Sub
 End Class
